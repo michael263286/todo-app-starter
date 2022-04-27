e446fedc67023a0d92ba5de6430ea898082f35c3
Michael Nguyen <michael263286@gmail.com>
2022-04-27 09:17:20 -0500

    finished

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 // comes before 'script.js' in the HTML document
 console.log(todoItems)
 
+const completeList = document.querySelector('#complete-items')
+const incompleteList = document.querySelector('#incomplete-items')
+
+
+for (let item of todoItems){
+
+    const listItem = document.createElement('li')
+    listItem.innerText = item.title
+    listItem.classList.add('list-group-item')
+    listItem.classList.add('form')
+    if (item.completed == true){
+        completeList.appendChild(listItem)
+    }
+    else{
+        incompleteList.appendChild(listItem)
+    }
+  }
+
 // Use the 'todoItems' variable to create the appropriate elements and append them onto
 // the page.
