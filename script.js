788a168839a9ffcdc609c892668c96a913e28581
Michael Nguyen <michael263286@gmail.com>
2022-05-03 11:17:31 -0500

    updated

--- script.js
+++ script.js
@@ -4,21 +4,80 @@ console.log(todoItems)
 
 const completeList = document.querySelector('#complete-items')
 const incompleteList = document.querySelector('#incomplete-items')
+const newTodo = document.querySelector('#todoForm')
+const inputItem = document.querySelector('#inputItem')
 
 
-for (let item of todoItems){
 
-    const listItem = document.createElement('li')
-    listItem.innerText = item.title
-    listItem.classList.add('list-group-item')
-    listItem.classList.add('form')
-    if (item.completed == true){
-        completeList.appendChild(listItem)
+newTodo.addEventListener('submit',function(e){
+    e.preventDefault()
+    const newTask = {
+        id: todoItems.length + 1,
+        title: inputItem.value,
+        completed:false,
+    }
+
+    todoItems.push(newTask)
+    console.log(todoItems)
+    const addTaskInto = document.createElement('li')
+    addTaskInto.classList.add('list-group-item')
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.classList.add('form-check-input')
+    addTaskInto.appendChild(checkbox)
+    const label = document.createElement('label')
+    label.innerText = newTask.title
+    label.classList.add('form-check-label','ps-3')
+    addTaskInto.appendChild(label)
+    incompleteList.appendChild(addTaskInto)
+    const removeItem = document.createElement('button')
+    addTaskInto.appendChild(removeItem)
+    removeItem.innerText = 'Remove'
+
+    removeItem.addEventListener('click',function(event){
+        addTaskInto.remove()
+    })
+
+checkbox.addEventListener('click',function(){
+    if(checkbox.checked){
+        completeList.appendChild(addTaskInto)
+        todoItems.completed
     }
     else{
-        incompleteList.appendChild(listItem)
+        incompleteList.appendChild(addTaskInto)
+        
     }
-  }
+
+})
+
+
+
+
+    newTodo.reset()
+    
+})
+
+
+// for (let item of todoItems){
+//     const listItem = document.createElement('li')
+//     listItem.classList.add('list-group-item')
+//     const checkbox = document.createElement('input')
+//     checkbox.type = 'checkbox'
+//     checkbox.classList.add('form-check-input')
+//     listItem.appendChild(checkbox)
+//     const label = document.createElement('label')
+//     label.innerText = item.title
+//     label.classList.add('form-check-label','ps-3')
+//     listItem.appendChild(label)
+
+//     if (item.completed){
+//         completeList.appendChild(listItem)
+//         checkbox.checked = true
+//     }
+//     else{
+//         incompleteList.appendChild(listItem)
+//     }
+// }
 
 // Use the 'todoItems' variable to create the appropriate elements and append them onto
 // the page.
